3475807b1f80e4f0a3f019179d34aefa6544be37
HC-Strong <stronghannahc@gmail.com>
2019-09-07 17:41:47 -0700

    Lesson 04: Setup of layout for user create activity

--- app/src/main/java/com/example/smack/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/CreateUserActivity.kt
@@ -1,27 +1,30 @@
 package com.example.smack
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 
-class LoginActivity : AppCompatActivity() {
+class CreateUserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_create_user)
     }
 
-    fun loginLoginBtnClicked(view: View) {
+    fun generateUserAvatar(view: View){
+        Log.d("view", "view tag is ${view.tag}") // don't actually care and never set it
+        //but this makes the warnings about not using 'view' stop
+
+    }
+
+    fun generateColorClicked(view: View) {
         Log.d("view", "view tag is ${view.tag}") // don't actually care and never set it
         //but this makes the warnings about not using 'view' stop
     }
 
-    fun loginCreateUserBtnClicked(view: View) {
+    fun createUserClicked(view: View) {
         Log.d("view", "view tag is ${view.tag}") // don't actually care and never set it
         //but this makes the warnings about not using 'view' stop
-        val createUserIntent = Intent(this, CreateUserActivity::class.java)
-        startActivity(createUserIntent)
     }
 }
